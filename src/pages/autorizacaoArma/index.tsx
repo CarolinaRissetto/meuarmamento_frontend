2c429a3c0688f05dcb20925d9be74ba82b9639bf
Carolina Rissetto <carolinarrissetto@gmail.com>
2024-10-09 19:46:54 -0300

    Ajustando foco no input nome ao clicar em novo processo

--- src/pages/autorizacaoArma/index.tsx
+++ src/pages/autorizacaoArma/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useMemo, useCallback, useRef, RefObject } from "react";
+import React, { useEffect, useState, useMemo, useCallback, useRef } from "react";
 import { useNavigate, useLocation } from "react-router-dom";
 import {
   Button,
